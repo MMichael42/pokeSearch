74a48399ab5a7d01f0f1d1a6cfe2ae512026bacc
MMichael42 <michaeld.johnsoncst@gmail.com>
2018-11-08 10:08:09 -0800

    cleaned up a little bit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let searchBar = document.getElementById('searchBar');
 let dropDown = document.getElementById('setDropdownWrap');
 let dropDownList = document.getElementById('setDropdown');
 let searchResultsDiv = document.getElementById('searchResults');
+
 let pokemonAPIurlString = "https://api.pokemontcg.io/v1/";
 let userInput = "";
 let selectedSetCode = "";
@@ -12,53 +13,50 @@ function searchPokes(event) {
   let key = event.data;
   let inputType = event.inputType;
 
-  if (inputType != "insertText" && inputType != "deleteContentBackward") {
+  if (inputType !== "insertText" && inputType !== "deleteContentBackward") {
     // do nothing
     return;
-  } else if (inputType == "deleteContentBackward" && userInput != "") {
+  } else if (inputType === "deleteContentBackward" && userInput !== "") {
     // backspace and search string is not empty, delete last character
     userInput = userInput.slice(0, -1);
     // since we're changing the pokemon's name we're searching, reset the setCode stuff so searhAPI re runs the set selecting code
     selectedSetCode = "";
     setSelected = false;
-  } else if (inputType == "insertText") {
+  } else if (inputType === "insertText") {
     // add to string
     userInput += key;
   }
 
   // we're empty searching, so also clear out everything, reset the setSelected state
-  if (searchBar.value == "") {
+  if (searchBar.value === "") {
     userInput = "";
     selectedSetCode = "";
     setSelected = false;
   }
 
-  // good to go, send off userInput to the API:
-  if (userInput != "") { // search if the string isn't empty
+  // good to go, send off userInput and selectedSetCode to the API:
+  if (userInput !== "") { // search if the string isn't empty
     // submit to search
     searchAPI(userInput, selectedSetCode);
     // and show the set dropdown list
     dropDown.style.display = "block";
-  } else if (userInput == "") {
+  } else if (userInput === "") {
     dropDown.style.display = "none";
     clearDiv(searchResultsDiv);
   }
 } //  end of searchPokes func
 
-function clearDiv(div) {
-  console.log('empty out the div');
-  div.innerHTML = "";
-} // enf of clearDiv func
-
-
 function searchAPI(pokemonName, setCode) {
-  if (pokemonName == "") { // after we load results of last search, see if the search String is currently "", if so clear out div.
+  let fullURL = pokemonAPIurlString + 'cards?name=' + pokemonName + '&setCode=' + setCode;
+
+  if (pokemonName === "") { // after we load results of last search, see if the search String is currently "", if so clear out div.
     console.log("empty search string from inside searchAPI");
     clearDiv(searchResultsDiv);
     clearDiv(dropDownList);
     return;
   }
-  fetch(pokemonAPIurlString + 'cards?name=' + pokemonName + '&setCode=' + setCode)
+  
+  fetch(fullURL)
     .then( response => {
       return response.json();
     })
@@ -93,7 +91,7 @@ function searchAPI(pokemonName, setCode) {
         });
         dropDownList.innerHTML = '<option value="" selected>All Sets</option>' + dropDownSetItem;
       }
-      if (searchBar.value == "") { 
+      if (searchBar.value === "") { 
         clearDiv(searchResultsDiv);
         clearDiv(dropDownList);
       }
@@ -114,3 +112,9 @@ function setSelect() {
   searchAPI(userInput, selectedSetCode);
 }
 
+function clearDiv(div) {
+  if (div !== "") {
+    console.log('empty out the div');
+    div.innerHTML = "";
+  }
+} // enf of clearDiv func
